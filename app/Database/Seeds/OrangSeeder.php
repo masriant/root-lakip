ebb40ac2f0b657225d3d15a4a3c457ecad0ebe3e
masriant <info.masrianto@gmail.com>
2023-08-10 18:14:25 +0700

    Using Query Builder
    Signed-off-by: masriant <info.masrianto@gmail.com>

--- app/Database/Seeds/OrangSeeder.php
+++ app/Database/Seeds/OrangSeeder.php
@@ -10,24 +10,46 @@ class OrangSeeder extends Seeder
 {
     public function run()
     {
-        $data = [
-            'nama'          => 'Masrianto',
-            'alamat'        => 'Jl. Serdang Baru No.4B Kemayoran - Jakarta',
-            'created_at'    => Time::now(),
-            'updated_at'    => Time::now(),
-        ];
-
         // -----------------------------------------------------------------------------------------------------------------
         // Simple Queries
         // -----------------------------------------------------------------------------------------------------------------
-        $this->db->query(
-            'INSERT INTO orang (nama, alamat, created_at, updated_at) VALUES(:nama:, :alamat:, :created_at:, :updated_at:)',
-            $data
-        );
+        // $data = [
+        //     'nama'          => 'Masrianto',
+        //     'alamat'        => 'Jl. Serdang Baru No.4B Kemayoran - Jakarta',
+        //     'created_at'    => Time::now(),
+        //     'updated_at'    => Time::now(),
+        // ];
+
+        // $this->db->query(
+        //     'INSERT INTO orang (nama, alamat, created_at, updated_at) VALUES(:nama:, :alamat:, :created_at:, :updated_at:)',
+        //     $data
+        // );
 
         // -----------------------------------------------------------------------------------------------------------------
         // Using Query Builder
         // -----------------------------------------------------------------------------------------------------------------
-        // $this->db->table('users')->insert($data);
+        $data = [
+            [
+                'nama'          => 'Masrianto',
+                'alamat'        => 'Jl. Serdang Baru Raya No.4B Kemayoran - Jakarta',
+                'created_at'    => Time::now(),
+                'updated_at'    => Time::now()
+            ],
+            [
+                'nama'          => 'Hasan Basri',
+                'alamat'        => 'Jl. Taruna Jaya No.44 Kemayoran - Jakarta',
+                'created_at'    => Time::now(),
+                'updated_at'    => Time::now()
+            ],
+            [
+                'nama'          => 'Muh Zainal',
+                'alamat'        => 'Jl. Cempaka Utara 4 No.25 Kemayoran - Jakarta',
+                'created_at'    => Time::now(),
+                'updated_at'    => Time::now()
+            ]
+        ];
+
+        // $this->db->table('orang')->insert($data);
+        $this->db->table('orang')->insertBatch($data);
     }
 }
