15c325bbf55f84490afd319b7e4d7dacc82ecb3c
masriant <info.masrianto@gmail.com>
2023-08-10 19:42:43 +0700

    Faker Data
    Signed-off-by: masriant <info.masrianto@gmail.com>

--- app/Database/Seeds/OrangSeeder.php
+++ app/Database/Seeds/OrangSeeder.php
@@ -10,15 +10,18 @@ class OrangSeeder extends Seeder
 {
     public function run()
     {
-        $faker = \Faker\Factory::create();
-        $data = [
-            'nama'          => $faker->name,
-            'alamat'        => $faker->address,
-            'created_at'    => Time::now(),
-            'updated_at'    => Time::now(),
-        ];
+        $faker = \Faker\Factory::create('id_ID');
+        for ($i = 0; $i < 100; $i++) {
+            $data = [
+                'nama'          => $faker->name('male' | 'female'),
+                // 'nama'          => $faker->name('male'),
+                'alamat'        => $faker->address,
+                'created_at'    => Time::createFromTimestamp($faker->unixTime()),
+                'updated_at'    => Time::now(),
+            ];
 
-        $this->db->table('orang')->insert($data);
+            $this->db->table('orang')->insert($data);
+        }
 
 
 
